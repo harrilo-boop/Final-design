e7dab299bfaf830b5549fda550d5530d556806f6
harrilo-boop <harri.cos.lo@gmail.com>
2026-06-08 14:07:46 +1200

    Attack system in overworld(progressing)
    Method for player to enter the battle by an attack area

---
+++ final-project/script/global.gd
@@ -0,0 +1,10 @@
+extends Node
+
+#All Variables for player in game
+var player_hp:int = 10
+var max_hp:int = 10
+
+var player_tp:int = 20
+var max_tp:int = 20
+
+var current_level:int = 0
--- final-project/script/player.gd
+++ final-project/script/player.gd
@@ -1,16 +1,74 @@
 extends CharacterBody2D
 
 var speed:float = 500
+var is_attacking: bool = false
+var can_attack: bool = true
+var last_direction: Vector2 = Vector2.RIGHT
+var hitbox_offset: Vector2
 
+@export var pivot: Marker2D
+@export var sword_area: Area2D
+@export var sword_collision: CollisionShape2D
+@export var timer: Timer
 
 func _ready() -> void:
-	pass
-
+	sword_area.monitoring = false
+	sword_collision.disabled = true
+	hitbox_offset = sword_area.position
 
 func _process(delta: float) -> void:
 	var direction: Vector2 = Vector2(0.0, 0.0)
 	direction.x = Input.get_axis("ui_left", "ui_right")
 	direction.y = Input.get_axis("ui_up", "ui_down")
 	
-	velocity = speed * direction.normalized()
+	if direction != Vector2.ZERO:
+		velocity = speed * direction.normalized()
+		last_direction = direction
+		update_hitbox_offset()
+	else:
+		velocity = Vector2.ZERO
 	move_and_slide()
+
+func update_hitbox_offset() -> void:
+	var dir = last_direction.normalized()
+	var distance = 80   
+	if abs(dir.x) > abs(dir.y):
+		if dir.x > 0:
+			sword_area.position = Vector2(distance, 0)     
+		else:
+			sword_area.position = Vector2(-distance, 0)    
+	else:
+		if dir.y > 0:
+			sword_area.position = Vector2(0, distance)     
+		else:
+			sword_area.position = Vector2(0, -distance)  
+
+func handle_attack() -> void:
+	if Input.is_action_just_pressed("ui_attack"):
+		_start_attack()
+
+func _start_attack() -> void:
+	is_attacking = true
+	sword_area.monitoring = true
+	sword_collision.disabled = false
+	timer.start()
+
+func end_attack() -> void:
+	sword_area.monitoring = false
+	sword_collision.disabled = true
+	
+	is_attacking = false
+
+func _on_sword_hit(body: Node) -> void:
+	if body == self:
+		return
+	if body.is_in_group("Enemy"):
+		_enter_battle()
+
+func _enter_battle() -> void:
+	get_tree().call_deferred("change_scene_to_file", "res://scenes/In_battle.tscn")
+	#Enter a battle and change the scene 
+
+
+func _attack_timeout() -> void:
+	end_attack()
