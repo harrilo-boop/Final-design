08748d8241b762b3e246d63f7d0b716bf0408df0
harrilo-boop <harri.cos.lo@gmail.com>
2026-08-28 13:35:10 +1200

    Update item_manager.gd

--- final-project/script/Global_script/item_manager.gd
+++ final-project/script/Global_script/item_manager.gd
@@ -12,15 +12,15 @@ func use_item(item:item_resource, battle):
 		item.EffectType.ATK_BUFF or item.EffectType.TECH_BUFF:
 			use_buff(item,battle)
 
-func use_heal(item,battle):
+func use_heal(item, battle):
 	battle.player_hp += item.heal_amount
 	battle.player_hp = min(battle.player_hp,battle.max_hp)
 	battle.player_tp += item.tp_amount
 	battle.player_tp = max(battle.player_tp, battle.max_tp)
 	
-func use_shield(item,battle):
+func use_shield(item, battle):
 	Global.shield_amount += item.defend_amount
 
-func use_buff(item,battle):
+func use_buff(item, battle):
 	battle.player_atk += item.atk_up
 	battle.total_damage_attack += item.tech_up
